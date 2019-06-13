263535a6ec891c05d1f172a666cec8b8dfdfbd95
Michael <51720724+mchappychen@users.noreply.github.com>
2019-06-13 10:26:32 -0400

    Finished Master-Plan outline

--- invertMatrix.py
+++ invertMatrix.py
@@ -75,15 +75,27 @@ def checkErrors(a):
 #returns [a,identity] updated
 def upperT(a,identity):
     """Plan:
+        xxxx    xxxx    xxxx    xxxx    xxxx    xxxx
+        xxxx  > xxxx  > xxxx  > 0xxx  > 0xxx  > 0xxx
+        xxxx    0xxx    0xxx    0xxx    00xx    00xx
+        0xxx    0xxx    00xx    00xx    00xx    000x
         
+        row := row - (current/above) * (above_row)
+        if current = 0, do nothing
     """
     return [a,identity]
 
 
 #returns [a,identity] updated
 def diag(a,identity):
-    """Plan:
+    """Plan:        
+        1xxx    1xxx    1xxx    1xxx
+        0xxx  > 01xx  > 01xx  > 01xx
+        00xx    00xx    001x    001x
+        000x    000x    000x    0001
         
+        row := (1/current) * row
+        if current = 1, do nothing
     """                     
     return [a,identity]
     
@@ -91,7 +103,13 @@ def diag(a,identity):
 #returns [a,identity] updated
 def lowerT(a,identity):
     """Plan:
-        
+        1xx0    1xx0    1x00    1x00    1x00    1000
+        01xx  > 01x0  > 01x0  > 01x0  > 0100  > 0100
+        001x    001x    001x    0010    0010    0010
+        0001    0001    0001    0001    0001    0001
+
+        row := row - (current/below) * (below_row)
+        if current = 0, do nothing
     """
     return [a,identity]
 
@@ -99,7 +117,28 @@ def lowerT(a,identity):
 #switches rows so that 0 is in bottom left or top right
 def switch(a,identity):
     """Plan:
+        For each step, start from the bottom-up
+        1. Check if an entire row has 0s, then it's un-invertible
+        2. Check if an entire row has 3 0s, position it at the right row, and lock the row
+            If there are 2 rows that need to get in the same row, it's un-invertible
+        3. Check if an entire row has 2 0s, place it not on diagonal, starting from bottom, lock the row
+            If the the row needs to place in a locked row, check above
+            If it's on a diagonal, check the row above
+            If you get to the top-most row, and it's locked, it's un-invertible
+        4. Check if an entire row has 1 0s, lock the row
+            If it's on a diagonal, check the row above
+            If the row needs to be place in a locked row, check above
+            If you get to the top-most row, and it's locked, it's un-invertible
+        5. Check the matrix if there's a number below a 0 below the diagonal
+            if there is, then it's un-invertible
+        6. Check the matrix if there's a number above a 0 above the diagonal
+            if there is, then it's un-invertible
+        7. Check if there's a 0 in the diagonal
+            if there is, you screwed up, since this should not be possible, and it's un-invertible
+        7. return the matrix
     """
+        return[a,identity]
+    
 
 def inverse(a):
     
@@ -122,32 +161,39 @@ def inverse(a):
     printAugmentedMatrix(a,identity)
     
     """Master Plan:
-        1. Swith rows until 0s are at the bottom or top of diagonal in switch()
-        2. Set the bottom-left triangle to 0 in upperT()
-        3. Set the diagonal to 1 in diag()
+        1. Switch
+        2. UpperT
+        3. Diag
+        4. LowerT
     """
     
-    #Step 4: Switch rows so that 0s 
-    #Step 4: Convert diagonal into 1
-    augment = diag(a,identity)
+    #1. Switch
+    augment = switch(a,identity)
     a = augment[0]
     identity = augment[1]
-    print("\nAfter diag(), augmented matrix looks like:\n")
+    print("\nAfter switch(), augmented matrix looks like:\n")
     printAugmentedMatrix(a,identity)
-    
-    #Step 5: Convert upper triangle into 0
+
+    #2. UpperT
     augment = upperT(a,identity)
     a = augment[0]
     identity = augment[1]
     print("\nAfter upperT(), augmented matrix looks like:\n")
     printAugmentedMatrix(a,identity)
     
-    #Step 6: Convert lower triangle into 0
+    #3. Diag
+    augment = diag(a,identity)
+    a = augment[0]
+    identity = augment[1]
+    print("\nAfter diag(), augmented matrix looks like:\n")
+    printAugmentedMatrix(a,identity)
+    
+    #4. LowerT
     augment = lowerT(a,identity)
     a = augment[0]
     identity = augment[1]
     print("\nAfter lowerT(), augmented matrix looks like:\n")
     printAugmentedMatrix(a,identity)
     
-    #Step 7: Return the updated Identity Matrix
+    #Step 4: Return the Identity Matrix
     return identity
