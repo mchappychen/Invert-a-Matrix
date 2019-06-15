47f3561ce0d0b7913ade230afd8d25e5b33c9989
Michael <51720724+mchappychen@users.noreply.github.com>
2019-06-15 16:44:41 -0400

    switch needs to be re-written from scratch

--- invertMatrix.py
+++ invertMatrix.py
@@ -171,73 +171,22 @@ def checkInverseMatrix(A_inverse,A): #checks if A-1 x A = I
         for y in range(len(A_inverse)):
             if(x == y):
                 if(round(A_inverse[x][y],2) != 1.00):
-                    print("Element at (",x,",",y,") of A-1xA is",A_inverse[x][y])
+                    print("A-1 x A Error: (",x,",",y,") is",round(A_inverse[x][y],2),"but should be 1")
                     success = False
             else:
                 if(round(A_inverse[x][y],2) != 0.0):
-                    print("Element at (",x,",",y,") of A-1xA is",A_inverse[x][y])
+                    print("A-1 x A Error: (",x,",",y,") is",round(A_inverse[x][y],2),"but should be 0")
                     success = False
     if(success):
         print("Successfully inverted matrix")
     else:
         print("Matrix inversion failed")
 def switch(a,identity):
-    """Plan:
-        For each step, start from the bottom-up
-        1. Check if an entire row has 0s, then it's un-invertible
-        2. Check if an entire row has n-1 0s, position it at the right row, and lock the row
-            If there are 2 rows that need to get in the same row, it's un-invertible
-        2.1 Check if an entire row has n-2 0s, place it not on diagonal, starting from bottom, lock the row
-            If the the row needs to place in a locked row, check above
-            If it's on a diagonal, check the row above
-            If you get to the top-most row, and it's locked, it's un-invertible
-        2.2 Check if an entire row has n-3 0s, lock the row
-            If it's on a diagonal, check the row above
-            If the row needs to be place in a locked row, check above
-            If you get to the top-most row, and it's locked, it's un-invertible
-        ...
-            
-        3. Check the matrix if there's a number below a 0 below the diagonal
-            if there is, then it's un-invertible
-        4. Check the matrix if there's a number above a 0 above the diagonal
-            if there is, then it's un-invertible
-        5. Check if there's a 0 in the diagonal
-            if there is, you screwed up, since this should not be possible, and it's un-invertible
+    """Plan: (to be worked on)
     """
-    lockedRows = []  
-    #1
-    for x in a:
-        all_zeros = True
-        for y in x:
-            if(y != 0):
-                all_zeros = False
-                break
-        if(all_zeros):
-            exit("Error in switch(): Your matrix has a row full of 0s")
-    #2
-    for zeros in range(len(a)-1,0,-1): # 4...3...2...1 (zeros)
-        for row_index in range(len(a)-1,-1,-1):  #row1...row2...row3...row4
-            row_zeros = 0
-            
-            #Set row_zeros
-            for column_index in range(len(a)):
-                if(a[row_index][column_index] == 0):
-                    row_zeros += 1
-            
-            #Swap rows if needed
-            if(row_zeros == zeros):
-                #try to put it in the bottom-most row, if not, go up, if at the top, un-invertible
-                for row_to_check_index in range(len(a)-1,-2,-1):
-                    if(row_to_check_index == -1):
-                        exit("The way 0s are positioned in row "+str(row_index)+" make it un-invertible")
-                    elif(a[row_index][row_to_check_index] != 0 and (row_to_check_index not in lockedRows)):
-                        temp = a[row_to_check_index]
-                        a[row_to_check_index] = a[row_index]
-                        a[row_index] = temp
-                        lockedRows.append(row_to_check_index)
-                        break
-    
-    for row_index in range(len(a)): #none of these should be executed
+ 
+    """checks for any mis-placed 0s:"""
+    for row_index in range(len(a)):
         for column_index in range(len(a[0])):
             #3. if above diag, check if element above 0 is 0 or non-existent, if not, then exit()
             if (row_index < column_index):
@@ -374,7 +323,7 @@ def main():
         for x in range(rows):
             row = []
             for y in range(rows):
-                row.append(round(random.random()*10+1,0))
+                row.append(round(random.random()*10,0))
             matrix.append(row)
         inverse(matrix)
     elif(response == "3"):
